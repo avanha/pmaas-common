2673dde1cba514f2c68a4139dab1a25db31e865d
Andre Vanha <andre@vanhas.com>
2026-05-08 11:31:31 -0600

    Added generic slice Filter function.

--- slices/util.go
+++ slices/util.go
@@ -9,3 +9,15 @@ func Apply[S any, V any](inputs []S, f func(entry *S) V) []V {
 
 	return results
 }
+
+func Filter[S any](inputs []S, predicate func(entry *S) bool) []S {
+	results := make([]S, 0, len(inputs))
+
+	for i := 0; i < len(inputs); i++ {
+		if predicate(&inputs[i]) {
+			results = append(results, inputs[i])
+		}
+	}
+
+	return results
+}
